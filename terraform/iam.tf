3df5c918cb847a0f4fb90d42cea4411dc7cd43a4
Hartnell Foster <hartnell.foster@googlemail.com>
2020-10-25 10:06:37 +0000

    Refactored IAM Assume Role Policy JSON

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_role" "lambda_role" {
   name               = "iam_for_lambda"
-  assume_role_policy = file("assume_role_policy.json")
+  assume_role_policy = file("iam/assume_role_policy.json")
 }
